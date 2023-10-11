a16d835b5438f5ac40cc91a066a51724422d416a
K-Basket <114851751+K-Basket@users.noreply.github.com>
2023-10-11 21:59:10 +0000

    Deploying to gh-pages from @ K-Basket/car-rental@5be83d7ca0b23256f357b9044476ffc1233fdc06 🚀

--- static/js/415.a501e8ec.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcar_rental=self.webpackChunkcar_rental||[]).push([[415],{415:function(e,n,r){r.r(n);var a=r(184);n.default=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)("h1",{children:"Home Pages"})})}}}]);
-//# sourceMappingURL=415.a501e8ec.chunk.js.map
